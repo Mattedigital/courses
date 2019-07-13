5f6c5a22684fc48591f5838644b8a60fa263b14b
Matte Fletcher <matte.digital@potatolondon.com>
2019-07-13 17:17:14 +0100

    Lights out exercise.

--- udemy-modern-react/lights-out/src/Board.js
+++ udemy-modern-react/lights-out/src/Board.js
@@ -30,18 +30,31 @@ import './Board.css';
  **/
 
 class Board extends Component {
-
+  static defaultProps = {
+    nrows: 5,
+    ncols: 5,
+    chanceLightsStartOn: 0.25
+  }
   constructor(props) {
     super(props);
 
-    // TODO: set initial state
+    this.state = {
+      hasWon: false,
+      board: this.createBoard()
+    }
   }
 
   /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
 
   createBoard() {
     let board = [];
-    // TODO: create array-of-arrays of true/false values
+    for(let y = 0; y < this.props.nrows; y++) {
+      let row = [];
+      for (let x = 0; x < this.props.ncols; x++) {
+        row.push(Math.random() < this.props.chanceLightsStartOn);
+      }
+      board.push(row);
+    }
     return board
   }
 
@@ -61,24 +74,50 @@ class Board extends Component {
       }
     }
 
-    // TODO: flip this cell and the cells around it
+    flipCell(y, x);
+    flipCell(y, x - 1);
+    flipCell(y, x + 1);
+    flipCell(y - 1, x);
+    flipCell(y + 1, x);
 
     // win when every cell is turned off
-    // TODO: determine is the game has been won
 
-    this.setState({board, hasWon});
+    let hasWon = board.every(row => row.every(cell => !cell));
+
+    this.setState({
+      board,
+      hasWon
+    });
   }
 
 
   /** Render game board or winning message. */
 
   render() {
+    if (this.state.hasWon) {
+      return <h1>You Won!</h1>;
+    }
 
-    // if the game is won, just show a winning msg & render nothing else
-
-    // TODO
-
-    // make table board
+    let tblBoard = [];
+    for(let y = 0; y < this.props.nrows; y++){
+      let row = [];
+      for(let x = 0; x < this.props.ncols; x++) {
+        let coord = `${y}-${x}`;
+        row.push(
+          <Cell
+            key={coord}
+            isLit={this.state.board[y][x]}
+            flipCellsAroundMe={() => this.flipCellsAround(coord)}
+          />
+        );
+      }
+      tblBoard.push(<tr key={y}>{row}</tr>)
+    }
+    return (
+      <table className="Board">
+        <tbody>{tblBoard}</tbody>
+      </table>
+    )
 
     // TODO
   }
